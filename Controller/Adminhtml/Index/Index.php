18582dea9f2e89fa814c73a9b868e0cfe42b5df8
eric <ejmm10.19@gmail.com>
2025-12-18 22:26:14 -0500

    Add admin menu and blank page for Alpina Recipes and Articles

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright © Alpina. All rights reserved.
+ */
+
+namespace Alpina\RecipesAndArticles\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Check for is allowed
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Alpina_RecipesAndArticles::manage');
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Alpina_RecipesAndArticles::recipes_articles');
+        $resultPage->getConfig()->getTitle()->prepend(__('Alpina Recipes and Articles'));
+        return $resultPage;
+    }
+}
